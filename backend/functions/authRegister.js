677918aa869402e07377feaba8233397e81f38b0
Jackson <kahowang3659@gmail.com>
2024-03-04 17:15:50 +1100

    trying to fix the pipline for the workflow

--- backend/functions/authRegister.js
+++ backend/functions/authRegister.js
@@ -6,29 +6,30 @@ const saltRounds = 10;
 async function authRegister(email, phone_no, username, password) {
   try {
     // checks if given email is registered
-		const existingEmail = await pool.query("select * from users where email = $1", [email]);
-		if (existingEmail.rows.length > 0) {
-			throw createError(400, "Email entered already in use.");
-		};
+    const existingEmail = await pool.query("select * from users where email = $1", [email]);
+    if (existingEmail.rows.length > 0) {
+      throw createError(400, "Email entered already in use.");
+    }
 
     // checks if given phone is already registered
     const existingPhoneNo = await pool.query("select * from users where phone_no = $1", [phone_no]);
     if (existingPhoneNo.rows.length > 0) {
       throw createError(400, "Phone number entered already in use.");
-    };
+    }
 
     // checks if username is already taken
     const existingUser = await pool.query("select * from users where username = $1", [username]);
     if (existingUser.rows.length > 0) {
       throw createError(400, "Username entered already in use.");
-    };
+    }
 
     // encrypts the password using bcrypt
     const encrypted = await bcrypt.hash(password, saltRounds);
 
     // inserts the new registered user into the database 
     let q = "INSERT INTO users (email, phone_no, username, password) VALUES ($1, $2, $3, $4) RETURNING *";
-    const registered = await pool.query(q, [email, phone_no, username, encrypted]);
+    // const registered = await 
+    pool.query(q, [email, phone_no, username, encrypted]);
     console.log('Successfully registered:', username);
 
     // gracefully catches error for unexpected occurences
