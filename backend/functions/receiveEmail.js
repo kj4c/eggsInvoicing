f76d2968c37c9e90b0f21fdd01e33fd16a01eb0c
Low Khye Jac <z5428854@ad.unsw.edu.au>
2024-03-06 00:14:09 +1100

    Fixed up the indentation spaces from 4 to 2

--- backend/functions/receiveEmail.js
+++ backend/functions/receiveEmail.js
@@ -2,21 +2,21 @@ const pool = require('../database/db');
 const createError = require('http-errors');
 
 async function receiveEmail(receiver, invoiceId) {
-    try {
-        const emailFound = await pool.query("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
-        if (emailFound.rows.length === 0) {
-            throw createError(400, "Email not received.");
-        } else {
-            console.log(emailFound);
-            return {
-                status: 200,
-                message: "Success"
-            };
-        }
-    } catch (error) {
-        console.error("Failed to receive email:", error.message);
-        throw error;
+  try {
+    const emailFound = await pool.query("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
+    if (emailFound.rows.length === 0) {
+      throw createError(400, "Email not received.");
+    } else {
+      console.log(emailFound);
+      return {
+        status: 200,
+        message: "Success"
+      };
     }
+  } catch (error) {
+    console.error("Failed to receive email:", error.message);
+    throw error;
+  }
 }
 
 module.exports = receiveEmail;
--- backend/tests/receiveEmail.test.js
+++ backend/tests/receiveEmail.test.js
@@ -3,36 +3,36 @@ const pool = require("../database/db");
 const { describe, beforeEach } = require("node:test");
 
 jest.mock("../database/db", () => ({
-    query: jest.fn()
+  query: jest.fn()
 }));
 
 describe("Receiving Email", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    })
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
 
-    it("should receive and notify user of email if the email exists and was sent", async () => {
-        const receiver = 'Tom';
-        const invoiceId = 123;
-        pool.query.mockResolvedValueOnce({ rows: [{receiver: 'Tom', invoice_id: 123}]});
-        const result = await receiveEmail('receiver', invoiceId);
-        expect(result).toEqual({status: 200, message: "Success"});
-        expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
-    });
+  it("should receive and notify user of email if the email exists and was sent", async () => {
+    const receiver = 'Tom';
+    const invoiceId = 123;
+    pool.query.mockResolvedValueOnce({ rows: [{receiver: 'Tom', invoice_id: 123}]});
+    const result = await receiveEmail('receiver', invoiceId);
+    expect(result).toEqual({status: 200, message: "Success"});
+    expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
+  });
 
-    it("cannot find the receiver", async () => {
-        const receiver = 'KAIKAIKAI'
-        const invoiceId = 123;
-        pool.query.mockResolvedValueOnce({rows: []});
-        expect(receiveEmail(receiver, invoiceId)).rejects.toThrow("Email not received.");
-        expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId])
-    });
+  it("cannot find the receiver", async () => {
+    const receiver = 'KAIKAIKAI'
+    const invoiceId = 123;
+    pool.query.mockResolvedValueOnce({rows: []});
+    expect(receiveEmail(receiver, invoiceId)).rejects.toThrow("Email not received.");
+    expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
+  });
 
-    it("cannot find the invoice_id", async () => {
-        const receiver = 'Tom'
-        const invoiceId = 123333;
-        pool.query.mockResolvedValueOnce({rows: []});
-        expect(receiveEmail(receiver, invoiceId)).rejects.toThrow("Email not received.");
-        expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId])
-    });
+  it("cannot find the invoice_id", async () => {
+    const receiver = 'Tom'
+    const invoiceId = 123333;
+    pool.query.mockResolvedValueOnce({rows: []});
+    expect(receiveEmail(receiver, invoiceId)).rejects.toThrow("Email not received.");
+    expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
+  });
 })
