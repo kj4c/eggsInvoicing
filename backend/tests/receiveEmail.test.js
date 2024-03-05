d29792ec1b3b0dc2c1cb36c037d31716b1dc9c71
Low Khye Jac <z5428854@ad.unsw.edu.au>
2024-03-06 00:07:02 +1100

    finished up the tests and linted

--- backend/tests/receiveEmail.test.js
+++ backend/tests/receiveEmail.test.js
@@ -13,25 +13,26 @@ describe("Receiving Email", () => {
 
     it("should receive and notify user of email if the email exists and was sent", async () => {
         const receiver = 'Tom';
-        const invoice_id = 123;
+        const invoiceId = 123;
         pool.query.mockResolvedValueOnce({ rows: [{receiver: 'Tom', invoice_id: 123}]});
-        expect(receiveEmail('receiver', invoice_id)).toBe({status: 200, message: "Success"});
+        const result = await receiveEmail('receiver', invoiceId);
+        expect(result).toEqual({status: 200, message: "Success"});
         expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
     });
 
     it("cannot find the receiver", async () => {
         const receiver = 'KAIKAIKAI'
-        const invoice_id = 123;
+        const invoiceId = 123;
         pool.query.mockResolvedValueOnce({rows: []});
-        expect(receiveEmail('receiver', invoice_id)).toBe({status: 400, message: "Email not received."});
+        expect(receiveEmail(receiver, invoiceId)).rejects.toThrow("Email not received.");
         expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId])
     });
 
     it("cannot find the invoice_id", async () => {
         const receiver = 'Tom'
-        const invoice_id = 123333;
+        const invoiceId = 123333;
         pool.query.mockResolvedValueOnce({rows: []});
-        expect(receiveEmail('receiver', invoice_id)).toBe({status: 400, message: "Email not received."});
+        expect(receiveEmail(receiver, invoiceId)).rejects.toThrow("Email not received.");
         expect(pool.query).toHaveBeenCalledWith("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId])
     });
 })
