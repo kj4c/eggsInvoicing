1b4908726da668b4b25b5ed74077d56dc261c35f
Low Khye Jac <z5428854@ad.unsw.edu.au>
2024-03-06 00:01:42 +1100

    Finished and passed the tests

--- backend/functions/receiveEmail.js
+++ backend/functions/receiveEmail.js
@@ -1,5 +1,24 @@
 const pool = require('../database/db');
+const createError = require('http-errors');
 
-async function receiveEmail(userId, invoiceId) {
-    
+async function receiveEmail(receiver, invoiceId) {
+    try {
+        const emailFound = await pool.query("SELECT receiver, invoice_id FROM send_invoice WHERE RECEIVER = $1 AND invoice_id = $2",[receiver, invoiceId]);
+        if (emailFound.rows.length === 0) {
+            throw createError(400, "Email not received.");
+        } else {
+            console.log(emailFound);
+            return {
+                status: 200,
+                message: "Success"
+            };
+        }
+    } catch (error) {
+        console.error("Failed to receive email:", error.message);
+        throw error;
+    }
 }
+
+receiveEmail('Tom', 123);
+
+module.exports = receiveEmail;
