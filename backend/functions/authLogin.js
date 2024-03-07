5ee3d777a8edee2e99cc892a71cef2ffc21ce78d
Winnie Moy <z5477798@ad.unsw.edu.au>
2024-03-07 12:44:56 +1100

    created authLogin function

---
+++ backend/functions/authLogin.js
@@ -0,0 +1,34 @@
+const pool = require('../database/db');
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
+
+async function authLogin(username, password) {
+  try {
+        
+    // checks if username exists in database
+    const existingUser = await pool.query("select * from users where username = $1", [username]);
+    if (existingUser.rows.length == 0) {
+      throw createError(400, "Username does not exist");
+    }
+  
+    // compares the encrypted password to password entered
+    const encrypted = existingUser.rows[0].hashed_password;
+      
+    const compare = await bcrypt.compare(password, encrypted);
+    if (!compare) {
+      throw createError(401, "Password is incorrect");
+    }
+
+    console.log("Successfully logged in:", existingUser.rows[0]);
+
+    return {
+      uid: existingUser.rows[0].uid
+    }
+    // gracefully catches error for unexpected occurences
+  } catch (error) {
+    console.error("Failed to login:", error.message);
+    throw error;
+  }
+}
+  
+module.exports = authLogin;
