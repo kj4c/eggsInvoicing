b8f12d7acd490399b28af48882a0806600e4d40a
Eric Choe <141006995+Mahtombo@users.noreply.github.com>
2024-03-07 17:25:47 +1100

    added testing for hasReceivedInvoiceId

--- backend/functions/hasReceivedInvoiceId.js
+++ backend/functions/hasReceivedInvoiceId.js
@@ -1,7 +1,7 @@
 const pool = require("../database/db");
 
 async function hasReceivedInvoiceId(invoiceId, recieverEmail) {
-  let q = "select * from sent_invoices where invoice_id = $1 and receiver_email = $2";
+  const q = "select * from sent_invoices where invoice_id = $1 and receiver_email = $2";
   const res = (await pool.query(q, [invoiceId, recieverEmail])).rows;
   if (res.length === 0 ) {
     return {message: "User has not received invoiceId = " + invoiceId};
