42dffb8708debd1fce844a06ba8eda1ec56263a7
Jackson <kahowang3659@gmail.com>
2024-03-08 21:59:16 +1100

    commiting all and commenting useless sentences

--- backend/functions/sendSmsFunction.js
+++ backend/functions/sendSmsFunction.js
@@ -7,21 +7,23 @@ const twilioPhoneNumber = '+610421487088';
 const client = new twilio(accountSid, authToken);
 
 async function sendSMS(from, to, body) {
-    try {
-        const message = await client.messages.create({
-            body: body,
-            from: twilioPhoneNumber, // Twilio number
-            to: to // Recipient's phone number
-        });
+  try {
+    const message = await client.messages.create({
+      body: body,
+      from: twilioPhoneNumber, // Twilio number
+      to: to // Recipient's phone number
+    });
 
-        console.log(`Message sent: ${message.sid}`);
+    console.log(`Message sent: ${message.sid}`);
 
-        // Here you can implement database logic similar to your email function
-        // For example, saving the SMS details to your database
+    // Here you can implement database logic similar to your email function
+    // For example, saving the SMS details to your database
 
-        return message.sid;
-    } catch (error) {
-        console.error('Failed to send SMS:', error);
-        throw error;
-    }
+    return message.sid;
+  } catch (error) {
+    console.error('Failed to send SMS:', error);
+    throw error;
+  }
 }
+
+module.exports = sendSMS;
