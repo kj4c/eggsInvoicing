a65bd6590a71056bb57bb7ddf1b71a8d4c27d92d
Eric Choe <141006995+Mahtombo@users.noreply.github.com>
2024-03-26 13:10:24 +1100

    implemented navbar

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as Notification } from './Notification';
+export { default as UserProfile } from './UserProfile';
