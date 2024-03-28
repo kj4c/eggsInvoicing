421ac7c82f8ab0f8fc5cf58f01c0a79170ad95ab
Low Khye Jac <z5428854@ad.unsw.edu.au>
2024-03-29 02:57:19 +1100

    worked on inbox for invoice receiving

--- frontend/src/pages/InvoiceReceiving.jsx
+++ frontend/src/pages/InvoiceReceiving.jsx
@@ -1,7 +1,30 @@
+import '../stylesheets/InvoiceReceiving.css';
 
 const InvoiceReceiving = () => {
     return (
-      <div>InvoiceReceiving</div>
+      <>
+        <div className='searchContainer'>
+          <p>Fetch Invoices</p>
+          <input type="text" className='inputBox'/>
+          <select>
+            <option value="ID">by Invoice ID</option>
+            <option value="Date">by Date</option>
+            <option value="DateRange">by Date Range</option>
+          </select>
+        </div>
+        <div className="inboxContainer">
+            <p className='header'>Invoice ID</p>
+            <p className='header'>Title</p>
+            <p className='header'>Sender</p>
+            <p className='header'>Type</p>    
+            <p className='header'>Time</p>
+          <p class="grid-item">1</p>
+          <p class="grid-item">What is good guys!</p>
+          <p class="grid-item">kahowang3659@gmail.com</p>
+          <p class="grid-item">Multiple JSON</p>
+          <p class="grid-item">23:59</p>
+        </div>
+      </>
     )
   }
   
