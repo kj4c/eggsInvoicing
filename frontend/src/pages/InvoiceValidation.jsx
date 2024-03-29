4af98c296a6d34a9e7d795a8d2653d683aa7f91c
Winnie Moy <z5477798@ad.unsw.edu.au>
2024-03-30 02:28:08 +1100

    basic validation frontend

--- frontend/src/pages/InvoiceValidation.jsx
+++ frontend/src/pages/InvoiceValidation.jsx
@@ -2,15 +2,43 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../stylesheets/InvoiceValidation.css'
 
-
-// eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MTE2OTI2NDMsImV4cCI6MTcxMTc3OTA0M30.UhmsU2VY9fa7gSDU8-haZGDF9PbMz70vQthhnuF-s9OMpm1OPwkaFIicE7OMcJeOWncgAsP6ytedFjPeWE58mA
-
 const InvoiceValidation = () => {
-  const [file, setFile] = useState(null);
-  const handleOnChange = (event) => {
-    const files = event.target.files;
-    console.log(files);
-  };
+  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [passed, setPassed] = useState('');
+  const [failed, setFailed] = useState('');
+
+  function handleOnChange(e) {
+    const target = e.target;
+    setFile(target.files[0]);
+    setFileName(target.files[0].name);
+  }
+
+  async function handleOnSubmit(e) {
+    if (file !== '') {
+      const login = getValidate();
+      console.log(login);
+      setIsSubmitting(true);
+    }
+  }
+
+  const formData = new FormData;
+  formData.append('file', file);
+
+  async function getValidate() {
+    await axios.post('https://invoice-seng2021-24t1-eggs.vercel.app/login', {
+      body: {
+        "username": "khyejac",
+        "password": "password"
+      }
+    }).then(res => {
+      console.log(res.data);
+      setIsSubmitting(false);
+    }).catch(err => {
+      console.log(err.data)
+    })
+  }
 
   return (
     <div>
@@ -23,13 +51,16 @@ const InvoiceValidation = () => {
             <input type="file" id='select-file' className='select-file' onChange={handleOnChange} accept='.xml'></input>
             <label htmlFor='select-file' className='select-button'>Select Files to Upload</label>
           </div>
-          <button className='valid-submit'>Submit</button>
-          <p className='validating-load'>Validating...</p>
-          <p className='valid-result'>Invoice Passed Validation</p>
-          <p className='valid-report'>Generate Validation Report</p>
+          {file ? <p className='valid-uploaded'>Uploaded: {fileName}</p> : <p className='valid-uploaded'>No File Uploaded.</p>}
+          {/* <p className={file !== 'ready' ? 'valid-uploaded' : 'valid-not-uploaded'}>Uploaded: {fileName} </p> */}
+          <button className='valid-submit' onClick={handleOnSubmit}>Submit</button>
+          {isSubmitting && <p className='validating-load'>Validating...</p>}
+          {passed && <p className='valid-result'>Invoice Passed Validation</p>}
+          {failed && <p className='valid-result'>Invoice Failed Validation</p>}
+          <h2 className='valid-report'>Generate Validation Report</h2>
           <div className='valid-report-container'>
-            <button className='valid-html'>HTML</button>
-            <button className='valid-pdf'>PDF</button>
+            <button className='valid-type'>HTML</button>
+            <button className='valid-type'>PDF</button>
           </div>
         </div>
       </div>
