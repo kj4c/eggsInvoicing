dcd627e9b9f37240c8b030bd605408bddaf7f425
Winnie Moy <z5477798@ad.unsw.edu.au>
2024-03-30 15:36:38 +1100

    profile frontend edits

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,28 +1,32 @@
 // import React from 'react'
 import '../stylesheets/UserProfile.css';
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const UserProfile = () => {
-  // const [userDetails, setUserDetails] = useState({
-  //   username: '',
-  //   email: '',
-  //   phone: ''
-  // });
+  const [userDetails, setUserDetails] = useState({
+    username: '',
+    email: '',
+    phone: ''
+  });
 
-  // async function getUserInfo() {
-  //   const uid = localStorage.getItem('uid');
-  //   await axios.get('https://invoice-seng2021-24t1-eggs.vercel.app/getInfo', {
-  //     uid
-  //   }).then(res => {
-  //     setUserDetails.username(res.data.username);
-  //     setUserDetails.email(res.data.email);
-  //     setUserDetails.phone(res.data.phone);
-  //   }).catch(err => {
-  //     console.log(err);
-  //   })
-  // }
+  async function getUserInfo() {
+    const uid = localStorage.getItem('uid');
+    try {
+      // maybe get uses somthign diff??
+      const response = await axios.get('https://invoice-seng2021-24t1-eggs.vercel.app/getUserInfo', {
+        uid
+      });
+      setUserDetails({
+        username: res.data.username,
+        email: res.data.email,
+        phone: res.data.phone
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
@@ -32,17 +36,17 @@ const UserProfile = () => {
         <hr className="solid" />
         <div className='info-containers'>
           <p className='user-name'>Username</p>
-          <p className='user-info'>sample_user</p>
+          <p className='user-info'>{userDetails.username}</p>
         </div>
         <hr className="solid" />
         <div className='info-containers'>
           <p className='email'>Email</p>
-          <p className='user-info'>sample@mail.com</p>
+          <p className='user-info'>{userDetails.email}</p>
         </div>
         <hr className="solid" />
         <div className='info-containers'>
           <p className='phone-no'>Phone</p>
-          <p className='user-info'>0123456789</p>
+          <p className='user-info'>{userDetails.phone_no}</p>
         </div>
         <hr className="solid" />
         <Link to="/login" className='log-out'>Log Out</Link>
