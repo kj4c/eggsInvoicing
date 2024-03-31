aea2072201c46971e8a04a0bf01d1f91c6561bd6
cherisechan <cherisechan@outlook.com.au>
2024-03-31 14:21:47 +1100

    added notif and style sheet

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../components';
 import { cardData } from '../data/dashboardData';
@@ -10,34 +10,55 @@ const API_KEY = "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";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState('');
-
+  const [data, setData] = useState(null);
+  const [uid, setUid] = useState('');
+  const [name, setName] = useState('');
   const cookieExists = document.cookie.includes('cookie='); 
+  
   useEffect(() => {
     if (!cookieExists) {
       navigate('/login');
-      return;
-    }
-    let uid;
-    if (cookieExists) {
-      uid = document.cookie.split("; ");
-      uid = uid.find(part => part.startsWith("uid=")).split("=")[1];
-    }
-
-    if (data === '') {
-      notif(uid);
+    } else {
+      let id = document.cookie.split("; ");
+      id = id.find(part => part.startsWith("uid=")).split("=")[1];
+      setUid(id);
     }
   }, [cookieExists, navigate]);
 
-  // Page
-  const notif = async (uid) => {
-    try {
-      const response = await axios.get(`http://localhost:3000/receive/getNotifications?uid=${uid}`);
-      setData(`${response.data.notifications.length}`);
-    } catch (error) {
-      console.error(error);
+  const hasFetchedData = useRef(false);
+  useEffect(() => {
+    const notif = async () => {
+      try {
+        if (!uid) return;
+        console.log(uid);
+        const response = await axios.get(`https://invoice-seng2021-24t1-eggs.vercel.app/receive/getNotifications?uid=${uid}`);
+
+        setData(`${response.data.notifications.length}`);
+      } catch (error) {
+        console.error(error);
+      }
+    }; 
+
+    const info = async() => {
+      try {
+        const res = await axios.get('https://invoice-seng2021-24t1-eggs.vercel.app/getUserInfo', {
+          params: {
+            uid: uid
+          }
+        });
+        setName(res.data.username);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  };  
+  
+    if (uid !== '' && hasFetchedData.current === false) {
+      notif();
+      info();
+      hasFetchedData.current = true;
+    }
+  }, [uid]);
+  // Page 
 
   //chat
   const [messages, setMessages] = useState([
@@ -111,40 +132,18 @@ const Dashboard = () => {
   return (
     <div className='mt-12'>
       <div className='Dashboard lg-flex-nowrap'>
-        <div className='Dashboard-banner lg-w-80'>
-          <div className='Dashboard-Banner-Content'>
-            <div>
-              <p className='Dashboard-Earnings'>FY2024 Earnings</p>
-              <p className='Dashboard-Earnings-Content'>${data}</p>
-            </div>
+        <div className="welcome-page">
+          <div className="welcome-message">
+            <h1 className='welcome'>Welcome</h1>
+            <h1 className="name">{name}</h1>
           </div>
-          <div className='mt-6'>
-            <Button
-              color="white"
-              bgColor="blueviolet"
-              text="Download Report"
-              borderRadius="10px"
-            />
+          <div className='unread-box'>
+            <p className="unread-start">You have</p>
+            <p className="unread-data">{data}</p>
+            <p className="unread-end">unread emails</p>
           </div>
         </div>
 
-        <div className='Dashboard-Card-Container '>
-          {cardData.map((item) => (
-            <div key={item.title} className='Dashboard-Card-Style md-w-56'>
-              <button
-                type="button"
-                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className="Dashboard-Card-Button-Style"
-              >
-                {item.icon}
-              </button>
-              <p style={{marginTop: '0.75rem'}}>
-                <span className="">amount</span>
-              </p>
-              <p className="Dashboard-Card-Title">{item.title}</p>
-            </div>
-          ))}
-        </div>
         <div style={{ position: "fixed", bottom: "20px", right: "20px", height: "400px", width: "300px" }}>
         <MainContainer>
           <ChatContainer>
