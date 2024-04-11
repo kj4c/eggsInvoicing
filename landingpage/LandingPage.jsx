fb0e1738de4d11402a4587976be9808dbdedd127
Eric Choe <141006995+Mahtombo@users.noreply.github.com>
2024-04-12 00:57:35 +1000

    Header

---
+++ landingpage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header"
+import './stylesheets/LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <div className="landingPageTheme">
+      <div className="headerContainer">
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
