f5d1c4f610e57b4c647c571f2cde2263dbbd44c9
Eric Choe <141006995+Mahtombo@users.noreply.github.com>
2024-04-13 02:37:02 +1000

    removed unuses assets

--- frontend/src/data/Features.jsx
+++ frontend/src/data/Features.jsx
@@ -1,5 +1,3 @@
-import featureImage2 from '../assets/features/image-2.png';
-
 import { FaFileInvoice } from 'react-icons/fa';
 import { GrValidate } from 'react-icons/gr';
 import { ImFilePicture } from 'react-icons/im';
@@ -14,7 +12,6 @@ export const features = [
     text: 'Generate professional invoices through a user-friendly GUI form or optionally upload a JSON or CSV file.',
     backgroundUrl: './src/assets/features/card-1.svg',
     icon: <FaFileInvoice />,
-    imageUrl: featureImage2,
     url: '#create',
     colour: '#5ee1ea',
   },
@@ -24,7 +21,6 @@ export const features = [
     text: 'Ensure compliance and eliminate errors with automated validation against PEPPOL and syntax rules.',
     backgroundUrl: './src/assets/features/card-2.svg',
     icon: <GrValidate />,
-    imageUrl: featureImage2,
     light: true,
     url: '#validate',
     colour: '#FFC876',
@@ -35,7 +31,6 @@ export const features = [
     text: 'Receive XML invoices and instantly generate professionally formatted invoices with a click.',
     backgroundUrl: './src/assets/features/card-3.svg',
     icon: <ImFilePicture />,
-    imageUrl: featureImage2,
     url: '#render',
     colour: '#AC6AFF',
   },
@@ -45,7 +40,6 @@ export const features = [
     text: 'Send XML or JSON invoices via email, individually or in bulk, with ease and convenience - all in a few clicks.',
     backgroundUrl: './src/assets/features/card-4.svg',
     icon: <SlPaperPlane />,
-    imageUrl: featureImage2,
     light: true,
     url: '#send',
     colour: '#7ADB78',
@@ -56,7 +50,6 @@ export const features = [
     text: `View and download all your received invoices conveniently. Use our fetching feature to search invoices by ID, date, or date range for efficient organization. We've got everything you need for organized invoicing.`,
     backgroundUrl: './src/assets/features/card-5.svg',
     icon: <HiOutlineFolderOpen />,
-    imageUrl: featureImage2,
     url: '#receive',
     colour: '#FF776F',
   },
@@ -66,7 +59,6 @@ export const features = [
     text: 'We prioritize the security of your invoices. Our platform employs robust security measures to ensure that your sensitive information is protected at all times.',
     backgroundUrl: './src/assets/features/card-6.svg',
     icon: <MdOutlineSecurity />,
-    imageUrl: featureImage2,
     url: '#something',
     colour: '#ea5ed0',
   },
