f21a8b4058249a4453440941a09cebd4dcb8f227
cherisechan <cherisechan@outlook.com.au>
2024-04-14 03:12:21 +1000

    fixed test

--- backend/tests/teamDetail.test.js
+++ backend/tests/teamDetail.test.js
@@ -18,18 +18,20 @@ describe('team detail tests', () => {
   });
 
   it('email is not in a team', async () => {
+    pool.query.mockResolvedValueOnce({ rows: [{email: 'hi@gmail.com'}] });
     pool.query.mockResolvedValueOnce({ rows: [] });
     const res = await detailTeam('hi@gmail.com');
-    expect(pool.query).toHaveBeenCalledTimes(1);
+    expect(pool.query).toHaveBeenCalledTimes(2);
     expect(res.status).toEqual(400);
   });
 
   it('success', async () => {
+    pool.query.mockResolvedValueOnce({ rows: [{email: 'hi@gmail.com'}] });
     pool.query.mockResolvedValueOnce({ rows: [{teamid: 2}] });
     pool.query.mockResolvedValueOnce({ rows: [{email: 'hi@gmail.com'}] });
     pool.query.mockResolvedValueOnce({ rows: [{teamid: 2, teamname: 'hi', passcode: 'askjdhasd', teamemail: 'hi@team.com'}] });
     const res = await detailTeam('hi@gmail.com');
-    expect(pool.query).toHaveBeenCalledTimes(3);
+    expect(pool.query).toHaveBeenCalledTimes(4);
     expect(res.status).toEqual(200);
   });
 });
