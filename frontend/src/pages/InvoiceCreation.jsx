83fe4c3de0c6fba98c30e921d7c1ca51845a6948
Eric Choe <141006995+Mahtombo@users.noreply.github.com>
2024-04-13 14:00:04 +1000

    Removed default values and cleaned initial useState values from sharktank demonstration purposes

--- frontend/src/pages/InvoiceCreation.jsx
+++ frontend/src/pages/InvoiceCreation.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import "../stylesheets/InvoiceCreation.css";
-import { inputs } from "../data/InvoiceCreationInputs";
-import axios from "axios";
+import { useState } from 'react';
+import '../stylesheets/InvoiceCreation.css';
+import { inputs } from '../data/InvoiceCreationInputs';
+import axios from 'axios';
 
 // Invoice creationg GUI page where users can generate an invoice
 const InvoiceCreation = () => {
@@ -10,47 +10,47 @@ const InvoiceCreation = () => {
   // temporary set the values for the demo.
   const [values, setValues] = useState({
     invoice: {
-      inId: "123a",
-      date: "",
-      buyerRef: "1093A8",
+      inId: '',
+      date: '',
+      buyerRef: '',
       payment: {
-        pid: "109",
-        financialAccId: "1234567890",
-        accName: "Acc Name",
-        bsb: "123456",
+        pid: '',
+        financialAccId: '',
+        accName: '',
+        bsb: '',
       },
     },
-    
+
     seller: {
-      id: "13 004 085 616",
-      name: "EGG-Invoices Ltd",
+      id: '',
+      name: '',
       address: {
-        streetAdd: "123 Pitt St",
-        city: "Sydney",
-        postCode: "2000",
-        state: "NSW",
+        streetAdd: '',
+        city: '',
+        postCode: '',
+        state: '',
       },
     },
     customer: {
-      id: "53 004 085 616",
-      name: "UNSW",
+      id: '',
+      name: '',
       address: {
-        streetAdd: "123 Pitt St",
-        city: "Sydney",
-        postCode: "2000",
-        state: "NSW",
+        streetAdd: '',
+        city: '',
+        postCode: '',
+        state: '',
       },
       contact: {
-        name: "Jacqueline",
-        phone: "012345679",
-        email: "Jacquelinee@email.com",
+        name: '',
+        phone: '',
+        email: '',
       },
     },
     item: [],
   });
 
   // Download the generate file by creating a window
-  const downloadFile= (content, filename)  => {
+  const downloadFile = (content, filename) => {
     const blob = new Blob([content], { type: 'application/xml' });
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
@@ -60,7 +60,7 @@ const InvoiceCreation = () => {
     a.click();
     document.body.removeChild(a);
     window.URL.revokeObjectURL(url);
-  }
+  };
 
   // Creates the generated DIVs from increasing the item count.
   const handleGenerateDivs = (e) => {
@@ -69,15 +69,15 @@ const InvoiceCreation = () => {
     const value = parseInt(numItems);
     if (!isNaN(value) && value > 0) {
       const itemsArray = Array.from({ length: value }, () => ({
-        name: "",
-        itId: "",
-        unitPrice: "",
-        quantity: "",
-        taxCategory: "",
+        name: '',
+        itId: '',
+        unitPrice: '',
+        quantity: '',
+        taxCategory: '',
       }));
       setValues({ ...values, item: itemsArray });
     } else {
-      alert("Please enter a valid number greater than 0");
+      alert('Please enter a valid number greater than 0');
     }
   };
 
@@ -85,7 +85,10 @@ const InvoiceCreation = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://w13a-brownie.vercel.app/v2/api/invoice/create', values);
+      const res = await axios.post(
+        'https://w13a-brownie.vercel.app/v2/api/invoice/create',
+        values
+      );
 
       const xmlInvoice = res.data;
       downloadFile(xmlInvoice, 'Invoice.xml');
@@ -98,7 +101,6 @@ const InvoiceCreation = () => {
         console.error('Error setting up the request:', error.message);
       }
     }
-     
   };
 
   // Set the item values every new input
@@ -110,13 +112,13 @@ const InvoiceCreation = () => {
       const startIndex = name.indexOf('[') + 1;
       const endIndex = name.indexOf(']');
       const index = parseInt(name.substring(startIndex, endIndex));
-      const field = name.substring(endIndex + 2); 
+      const field = name.substring(endIndex + 2);
 
-      setValues(prevValues => {
+      setValues((prevValues) => {
         const updatedNumItems = [...prevValues.item];
         updatedNumItems[index] = {
-          ...updatedNumItems[index], 
-          [field]: value 
+          ...updatedNumItems[index],
+          [field]: value,
         };
         return { ...prevValues, item: updatedNumItems };
       });
@@ -124,35 +126,35 @@ const InvoiceCreation = () => {
     }
 
     if (field) {
-      setValues(prevState => ({
+      setValues((prevState) => ({
         ...prevState,
         [category]: {
           ...prevState[category],
           [subCategory]: {
             ...prevState[category][subCategory],
-            [field]: value
-          }
-        }
+            [field]: value,
+          },
+        },
       }));
     } else {
-      setValues(prevState => ({
+      setValues((prevState) => ({
         ...prevState,
         [category]: {
           ...prevState[category],
-          [subCategory]: value
-        }
+          [subCategory]: value,
+        },
       }));
     }
   };
 
   // Actual GUI for the Invoice Creation part
   return (
-    <div className="IC-Container">
-      <form className="IC-Form" onSubmit={handleSubmit}>
-        <h1 className="IC-h1">Invoice Form</h1>
-        <h2 className="IC-h2">Invoice Details</h2>
+    <div className='IC-Container'>
+      <form className='IC-Form' onSubmit={handleSubmit}>
+        <h1 className='IC-h1'>Invoice Form</h1>
+        <h2 className='IC-h2'>Invoice Details</h2>
         {inputs.map((input) => (
-          <div key={input.id} className="FI-Container">
+          <div key={input.id} className='FI-Container'>
             {input.heading && <h2> {input.heading} </h2>}
             <label>{input.label}</label>
             <input
@@ -162,68 +164,67 @@ const InvoiceCreation = () => {
               value={values[input.name]}
               onChange={onChange}
               required={input.required}
-              defaultValue={input.placeholder}
-              />
+            />
           </div>
         ))}
 
         {/* INVOICE LINE ITEMS */}
-        <div className="FI-Container">
+        <div className='FI-Container'>
           <h2>Invoice Line Items</h2>
-          <label className="label"> Number of Items </label>
+          <label className='label'> Number of Items </label>
           <input
-            type="number"
+            type='number'
             value={numItems}
             onChange={(e) => setNumItems(e.target.value)}
           />
-          
+
           {!buttonClicked && (
-            <button className="IC-button" onClick={handleGenerateDivs}>
+            <button className='IC-button' onClick={handleGenerateDivs}>
               Select
             </button>
           )}
 
           {values.item.map((item, index) => (
-            <div key={item+index} className="FI-Container">
-              <input 
-                label="Item Name"
+            <div key={item + index} className='FI-Container'>
+              <input
+                label='Item Name'
                 name={`item[${index}].name`}
-                placeholder="Orange"
+                placeholder='Orange'
                 onChange={onChange}
-                defaultValue="Orange"
+                defaultValue='Orange'
               />
               <input
-                label="Item ID"
+                label='Item ID'
                 name={`item[${index}].itId`}
-                placeholder="194mn21"
+                placeholder='194mn21'
                 onChange={onChange}
-                defaultValue="194mn21"
+                defaultValue='194mn21'
               />
               <input
-                label="Unit Price"
+                label='Unit Price'
                 name={`item[${index}].unitPrice`}
-                placeholder="3.99"
+                placeholder='3.99'
                 onChange={onChange}
-                defaultValue="3.99"
+                defaultValue='3.99'
               />
               <input
-                label="Quantity"
+                label='Quantity'
                 name={`item[${index}].quantity`}
-                placeholder="12"
+                placeholder='12'
                 onChange={onChange}
-                defaultValue="12"
+                defaultValue='12'
               />
               <input
-                label="Tax Category"
+                label='Tax Category'
                 name={`item[${index}].taxCategory`}
-                placeholder="Z"
+                placeholder='Z'
                 onChange={onChange}
-                defaultValue="Z"
+                defaultValue='Z'
               />
             </div>
           ))}
         </div>
-        <button className="IC-button">Submit</button>
+        <button className='IC-button'>Submit</button>
       </form>
     </div>
   );
