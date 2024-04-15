5aa55b5046dc94e91d62684c4d1b044b46fee388
Arnold45202 <kahowang3659@gmail.com>
2024-04-15 12:53:26 +1000

    fixing linting issues

--- frontend/src/pages/TeamCreate.jsx
+++ frontend/src/pages/TeamCreate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import SendImage from '../assets/send_imagev2.png';
